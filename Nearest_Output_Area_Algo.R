b18200c818453abc88e9305f1387e422acac5cd2
futurologist <58040154+futurologist@users.noreply.github.com>
2020-07-07 15:29:37 -0400

    Update Nearest_Output_Area_Algo.R

--- Nearest_Output_Area_Algo.R
+++ Nearest_Output_Area_Algo.R
@@ -2,8 +2,8 @@ library(dplyr)
 library(data.table)
 
 #########################################################################################################
-####
 
+# a function that relables the headers of the table initially extracted from the UKBiobank
 relabel <- function(table, fields, arrays_length, instances, labels){
   headers <- names(table)
   #n_headers <- length(headers)
@@ -37,7 +37,10 @@ relabel <- function(table, fields, arrays_length, instances, labels){
 }
 
 #########################################################################################################
+################################## functions for UKB data extraction: ###################################
 
+
+# extracts a relevant subtable of the raw UKB table, used in generate_location_table 
 extract_UKB_subtable <- function(data_base, list_of_codes){
   headers <- names(data_base)
   pos <- c(1)
@@ -49,99 +52,76 @@ extract_UKB_subtable <- function(data_base, list_of_codes){
   return(subt)
 }
 
+# bulids and labels the complete table for home location at birth, 
+# at visits 0,1,2 and date and home location history
+generate_location_table <- function(filepath_in){
+  codes <- c(130, 129, 20074, 20075, 20118, 
+             22700, 22702, 22704, 20115)
+  labels <- c('Brth_plc_east', 'Brth_plc_north', 'Home_loc_assess_east', 'Home_loc_assess_north', 'Urban_Rural', 
+              'Date_1st_at_loc', 'home_loc_east', 'home_loc_north', 'Country_brth')
+  fields <- paste('f.', codes, sep="")
+  data_base <- fread(filepath_in)
+  data_base <- extract_UKB_subtable(data_base, fields)
+  names(data_base)[1] <- 'IID'
+  array_length <- rep (1, length(fields))
+  array_length[which(grepl('f.227', fields))] <- 15
+  instances   <-   rep (1, length(fields))
+  instances[which(grepl('f.129', fields))] <- 3
+  instances[which(grepl('f.130', fields))] <- 3
+  instances[which(grepl('f.20074', fields))] <- 3
+  instances[which(grepl('f.20075', fields))] <- 3
+  names(data_base) <- relabel(data_base, fields, array_length, instances, labels)
+  return(data_base)
+}
 
-# For a set of fields that need to be extracted from the central UKB data table
-# list a vector of strings, each string in the format "f.[Field_number].", for each field.
-# Skip the first column, id column "f.eid", it is authomatically included
+# extracts north-east coordinates of home location at birth, visit 0, 
+extract_birth_location <- function(ukb_table){
+  table_out <- select(ukb_table, IID, Brth_plc_east_v0, Brth_plc_north_v0)
+  table_out <- filter(table_out, !is.na(ukb_table$Brth_plc_east_v0) &  ukb_table$Brth_plc_east_v0 !=-1)
+  return(as.data.table(table_out))
+}
 
-################################### YOUR INPUT GOES HERE: ###############################################
+# extracts north-east coordinates of home location at assessment, visit 0, 
+# exctracts the category of urbanization 
+# classifies urban rural locations base on category of urbanization
+extract_assess_location <- function(ukb_table){
+  ukb_curr_loc <- select(ukb_table, IID, Home_loc_assess_east_v0, Home_loc_assess_north_v0, Urban_Rural)
+  ukb_curr_loc <- filter(ukb_curr_loc, !is.na(ukb_curr_loc$Home_loc_assess_east_v0))
+  ukb_curr_loc <- mutate(ukb_curr_loc, 
+                         Urban = ifelse(is.na(ukb_curr_loc$Urban_Rural) | ukb_curr_loc$Urban_Rural == 9, NA, 
+                                        ifelse(ukb_curr_loc$Urban_Rural %in% c(5, 11, 12), 1, 0)))
+  
+  return(as.data.table(ukb_curr_loc))
+}
 
-codes <- c(130, 129, 20074, 20075, 20118, 
-           22700, 22702, 22704, 20115)
-labels <- c('Brth_plc_east', 'Brth_plc_north', 'Home_loc_asses_east', 'Home_loc_asses_north', 'Urban_Rural', 
-            'Date_1st_at_loc', 'home_loc_east', 'home_loc_north', 'Country_brth')
+###################################### Urban/Rural 1991 England and Wales: ##################################
 
+# excracts and puts together a table with
+# north-east coordinates of inhabited locations and their urban-rural classification in terms of categories 
+# the inhabited locations are 1991 wards for England and Wales, and 2001 ouput areas of Scotland  
+make_urban_rural_GB <- function(Eng_1991_ur, Wls_1991_ur, Scot_2001_oa, Scot_2003_ur){
+  en_1991 <- fread(Eng_1991_ur)
+  wl_1991 <- fread(Wls_1991_ur)
+  en_1991 <- select(en_1991,x,y,cat)
+  wl_1991 <- select(wl_1991,x,y,cat)
+  ew_1991 <- bind_rows(en_1991, wl_1991)
+  rm(en_1991)
+  rm(wl_1991)
+  sc_ur <- fread(Scot_2001_oa)
+  sc_ur <- select(sc_ur, c(2,3,4))
+  sc_ur_03 <- fread(Scot_2003_ur)
+  names(sc_ur_03)[1] <- 'name'
+  sc_ur <- left_join(sc_ur, sc_ur_03, by='name')
+  sc_ur <- select(sc_ur, x, y, UR8_2003_2004)
+  names(sc_ur)[3] <- c('cat') 
+  return(as.data.table(bind_rows(ew_1991, sc_ur)))
+}
 
-list_of_codes <- paste('f.', codes, sep="")
+######################## functions for Nearest Neighbour matching for England, Wales and Scotland: ########
 
-filepath_in <- "C:\\MY_FOLDERS\\Asthma_and_Pain\\UKB_raw_data\\ukb41654.r.tmp.tab" #string
-filepath_out <- "C:\\MY_FOLDERS\\Asthma_and_Pain\\UKB_raw_data\\ukb_loc_data.txt"
-
-#########################################################################################################
-##################################### Execute Relabeling of the headers #################################
-
-data_base <- fread(filepath_in)
-ukb_data <- extract_UKB_subtable(data_base, list_of_codes)
-#write.table(subtable, filepath_out, append = FALSE, sep = "\t", quote = FALSE, col.names=TRUE, row.names=FALSE)
-rm(data_base)
-
-####
-#########################################################################################################
-
-names(ukb_data)[1] <- 'IID'
-
-fields <- list_of_codes
-rm(list_of_codes)
-array_length <- rep (1, length(fields))
-array_length[which(grepl('f.227', fields))] <- 15
-instances   <-   rep (1, length(fields))
-instances[which(grepl('f.129', fields))] <- 3
-instances[which(grepl('f.130', fields))] <- 3
-instances[which(grepl('f.20074', fields))] <- 3
-instances[which(grepl('f.20075', fields))] <- 3
-
-names(ukb_data) <- relabel(ukb_data, fields, array_length, instances, labels)
-
-write.table(ukb_data, filepath_out, append = FALSE, sep = "\t", quote = FALSE, col.names=TRUE, row.names=FALSE)
-
-###################################### Urban/Rural for current location ###########################################
-ukb_data_1 <- fread(filepath_out)
-ukb_brt_loc <- as.data.table(filter(ukb_data, !is.na(ukb_data$Brth_plc_east_v0) &  ukb_data$Brth_plc_east_v0 !=-1))
-ukb_brt_loc <- select(ukb_brt_loc, IID, Brth_plc_east_v0, Brth_plc_north_v0)
-
-ukb_curr_loc <- select(ukb_data, 1, 8, 11, 14)
-ukb_curr_loc <- mutate(ukb_curr_loc, Urban = ifelse(ukb_curr_loc$Urban_Rural %in% c(5, 11, 12), 1, 
-                                                    ifelse(ukb_curr_loc$Urban_Rural == 9, -1, 0)))
-
-###################################### Urban/Rural 1991 England and Wales: ##################################################
-
-en_ur_1991 <- fread("C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\England_urbrur_classn_1991\\England_urbrur_classn_1991.csv")
-wl_ur_1991 <- fread("C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\Wales_urbrur_classn_1991\\wales_urbrur_classn_1991.csv")
-
-### England 1991:
-
-en_1991 <- select(en_ur_1991,x,y,cat)
-
-### Wales 1991:
-
-wl_1991 <- select(wl_ur_1991,x,y,cat)
-
-### merge England and Wales 1991 urban information 
-
-# England/Wales: Urban --> Rural <=> 1 --> 6
-
-ew_1991 <- bind_rows(en_1991, wl_1991)
-
-###########################  Urban/Rural 2001 Scotland:    ###########################
-
-sc_oa_cntr_01 <- fread('C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\Scotland_2001\\Scotland_oa_2001_gen3\\scotland_oa_2001_gen3.csv')
-sc_oa_cntr_01 <- select(sc_oa_cntr_01, c(2,3,4))
-
-sc_ur_oa <- fread('C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\Scotland_2001\\oa2001_urban_rural_2003_2004.csv')
-names(sc_ur_oa)[1] <- 'name'
-
-# Scotalnd: Urban --> Rural <=> 1 --> 6 or 8
-sc_ur <- left_join(sc_oa_cntr_01, sc_ur_oa, by='name')
-sc_ur <- select(sc_ur, x, y, UR8_2003_2004)
-sc_ur <- as.data.table(sc_ur)
-names(sc_ur)[3] <- c('cat') 
-
-###########################  Urban/Rural for England, Wales and Scotland: ###########################
-
-ews <- bind_rows(ew_1991, sc_ur)
-
-###########################  Nearest Neighbour Urban/Rural for England, Wales and Scotland: ##################
-
+# for a home-location with given north-east coordinates, 
+# finds the index of the nearest ward from England_Whalse_Scotland table of 
+# ward coordinates and their urban rural cathegory 
 index_of_nrst_nbr <- function(point_x, point_y, set_in){
   d_2 <- function(p_x, p_y){ return((p_x-point_x)^2 + (p_y-point_y)^2) }
   set <- mutate(set_in, distance_2 = d_2(set_in$x, set_in$y))
@@ -149,13 +129,22 @@ index_of_nrst_nbr <- function(point_x, point_y, set_in){
   return(i)
 }
 
+# by using the function index_of_nrst_nbr(),
+# adds a column of indexes where for each row, each index entry is the index of the 
+# ward from the England_Whalse_Scotland table of 
+# ward coordinates, nearest to the home-location listed in that row 
 add_col_of_idxs <- function(ukb_in, wards){
   ukb <- mutate(ukb_in, index = 0)
   nnb <- function(r){ return(index_of_nrst_nbr(as.double(r[2]), as.double(r[3]), wards)) }
   ukb[,'index'] <- as.data.table(apply(ukb, 1, nnb))
-  return(ukb)
+  return(as.data.table(ukb))
 }
 
+# takes the output from the funciton add_col_of_idxs(ukb_in, wards)
+# and adds and calculates the entries of columns
+# containing the coordiantes of the nearest ward, 
+# the urban-rural category of the nearest ward,
+# and the distance to the nearest ward,
 add_cols_of_coord_and_urb <- function(ukb_in, wards){
   M <- as.matrix(wards)
   nnb <- function(i,j,M){ return(M[i,j]) }
@@ -163,57 +152,68 @@ add_cols_of_coord_and_urb <- function(ukb_in, wards){
                 ward_north = nnb(index,2,M), 
                 category=nnb(index,3,M),
                 distance = sqrt(((Brth_plc_east_v0) - ward_east)^2 +
-                                ((Brth_plc_north_v0) - ward_north)^2)
+                                  ((Brth_plc_north_v0) - ward_north)^2)
                 #distance = sqrt(((Home_loc_asses_east_v0) - as.double(ward_east))^2 +
                 #                ((Home_loc_asses_north_v0) - as.double(ward_north))^2)
   )
-  return(ukb)
+  return(as.data.table(ukb))
 }
 
-start_time <- Sys.time()
-ukb_ur <- add_col_of_idxs(ukb_brt_loc, ews)
-end_time <- Sys.time()
-end_time - start_time
+# executes one after the other the functions 
+# add_col_of_idxs() and then
+# add_cols_of_coord_and_urb()
+# to rpoduce a final table with necessary data, ready for urban rural 
+# binary classification
+assign_nearest_ward <- function(ukb_brt_loc, ews){
+  start_time <- Sys.time()
+  ukb_ur <- add_col_of_idxs(ukb_brt_loc, ews)
+  end_time <- Sys.time()
+  print(end_time - start_time)
+  ukb_ur <- add_cols_of_coord_and_urb(ukb_ur, ews)
+  return(ukb_ur)
+}
 
-ukb_urb <- add_cols_of_coord_and_urb(ukb_ur, ews)
+#########################################################################################################
 
-write.table(ukb_urb, "C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\output_data\\UKB_urban_1991.txt", append = FALSE, sep = "\t", quote = FALSE, col.names=TRUE, row.names=FALSE)
+################################### YOUR INPUT GOES HERE: ###############################################
 
-###########################  Nearest Neighbour Urban/Rural for England, Wales and Scotland in 2011: ##################
+filepath_in <- 'E:\\Genomics\\Resources\\Output_data_ph1\\ukb41654.r.tmp.tab'  # "C:\\MY_FOLDERS\\Asthma_and_Pain\\UKB_raw_data\\ukb41654.r.tmp.tab" #string
+filepath_out <- 'E:\\Genomics\\Resources\\Output_data_ph2\\UKB_urban_1991.txt' # 'E:\\Genomics\\Resources\\Output_data_ph2\\ukb_loc_data.txt' #"C:\\MY_FOLDERS\\Asthma_and_Pain\\UKB_raw_data\\ukb_loc_data.txt"
 
-# REPLICATION:
+# bulids and labels the complete table for home location at birth, 
+# at visits 0,1,2 and date and home location history
+ukb_data <- generate_location_table(filepath_in)
 
-###################################### Urban/Rural 2011 England and Wales: ##################################################
+# extracts north-east coordinates of home location at birth, visit 0, 
+ukb_birth_loc <- extract_birth_location(ukb_data)
 
-en_ur_2011 <- fread("C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\England_oa_ru_classn_2011\\england_oa_ru_classn_2011.csv")
-wl_ur_2011 <- fread("C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\Wales_oa_ru_classn_2011\\wales_oa_ru_classn_2011.csv")
-
-en_2011 <- select(en_ur_2011,x,y,ruc11)
-
-### Wales 1991:
-
-wl_2011 <- select(wl_ur_2011,x,y,ruc11)
-
-cat <- distinct(en_2011, ruc11)
-names(en_2011)[3] <- 'cat'
-names(wl_2011)[3] <- 'cat'
+# extracts north-east coordinates of home location at assessment, visit 0, 
+# exctracts the category of urbanization 
+# classifies urban rural locations base on category of urbanization
+ukb_assess_loc <- extract_assess_location(ukb_data)
 
 
-### merge England and Wales 1991 urban information 
+filepath_Eng_1991_ur <- "C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\England_urbrur_classn_1991\\england_urbrur_classn_1991.csv"
+filepath_Wls_1991_ur <- "C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\Wales_urbrur_classn_1991\\wales_urbrur_classn_1991.csv"
+filepath_Scot_2001_oa <- 'C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\Scotland_2001\\Scotland_oa_2001_gen3\\scotland_oa_2001_gen3.csv'
+filepath_Scot_2003_ur <- 'C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\Scotland_2001\\oa2001_urban_rural_2003_2004.csv'
 
-# England/Wales: Urban --> Rural <=> 1 --> 6
+# excracts and puts together a table with
+# north-east coordinates of inhabited locations and their urban-rural classification in terms of categories 
+# the inhabited locations are 1991 wards for England and Wales, and 2001 ouput areas of Scotland  
+Eng_Wls_Scot <- make_urban_rural_GB(filepath_Eng_1991_ur, 
+                                filepath_Wls_1991_ur, 
+                                filepath_Scot_2001_oa, 
+                                filepath_Scot_2003_ur)
 
-ew_2011 <- bind_rows(en_2011, wl_2011)
+# takes the table with coordinates of birth home location from UKB 
+# and adds and calculates the entries of the new columns,
+# containing the coordiantes of the nearest ward, 
+# the urban-rural category of the nearest ward,
+# and the distance to the nearest ward
+ukb_ur_at_brth <- assign_nearest_ward(ukb_birth_loc, Eng_Wls_Scot)
 
-sc_ur[,'cat'] <- as.character(sc_ur$cat)
+# STILL NEED TO GENERATE THE BINARY URBAN RURAL CLASSIFICATION!!!  
 
-ews_11 <- bind_rows(ew_2011, sc_ur)
-
-start_time <- Sys.time()
-ukb_ur_11 <- add_col_of_idxs(filter(ukb_curr_loc, !is.na(ukb_curr_loc$Home_loc_asses_east_v0)), ews_11)
-end_time <- Sys.time()
-end_time - start_time
-
-ukb_urb_11 <- add_cols_of_coord_and_urb(ukb_ur_11, ews_11)
-
-write.table(ukb_urb_11, "C:\\MY_FOLDERS\\Asthma_and_Pain\\urban_rural\\output_data\\UKB_urban_2011.txt", append = FALSE, sep = "\t", quote = FALSE, col.names=TRUE, row.names=FALSE)
+write.table(ukb_ur_at_brth, filepath_out, 
+            append = FALSE, sep = "\t", quote = FALSE, col.names=TRUE, row.names=FALSE)
